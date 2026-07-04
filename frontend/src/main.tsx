a1770bc98bc360089c509a5f6cecdbee5a0cf4e5
kimyeji97 <kimyeji2035@gmail.com>
2026-07-04 16:30:07 +0900

    fix: App.css import 누락 반영

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import SettingsPanelProvider from 'providers/SettingsPanelProvider';
 import SettingsProvider from 'providers/SettingsProvider';
 import ThemeProvider from 'providers/ThemeProvider';
 import router from 'routes/router';
+import './App.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
